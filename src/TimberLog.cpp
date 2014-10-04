292aed2b67e689f3a83206d0aa5101e6133ef161
leeforaday <leonorflag@hotmail.com>
2014-10-04 17:12:02 +0100

    tronco feito

--- src/TimberLog.cpp
+++ src/TimberLog.cpp
@@ -7,4 +7,18 @@ TimberLog::~TimberLog() {
 }
 
 void TimberLog::draw() {
+	glColor3f(0.4f, 0.2f, 0.5f); //brown
+	glPushMatrix();
+	glScalef(3, 3, 1);
+	glRotatef(-45.0, 0.0, 1.0, 0.0);
+	glRotatef(-90.0, 1.0, 0.0, 0.0);
+	glutSolidTorus(.1, .13, 30, 100);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(.75, 0, 0);
+	glScalef(3, 3, 1);
+	glRotatef(-45.0, 0.0, 1.0, 0.0);
+	glRotatef(-90.0, 1.0, 0.0, 0.0);
+	glutSolidTorus(.1, .13, 30, 100);
+	glPopMatrix();
 }
